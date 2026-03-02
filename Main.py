50b65167c4a2c1182c18161aff5f6dcc1bcae733
Utsav0706 <utsavjivani.07@gmail.com>
2026-03-02 15:17:00 +0000

    Add method

--- Main.py
+++ Main.py
@@ -14,10 +14,15 @@ class Vector:
         self.x = x
         self.y = y
 
+    def __add__(self, other):
+
+        return Vector(self.x + other.x, self.y + other.y)
+
 
 
 v1 = Vector(10, 20)
 v2 = Vector(50, 60)
 v3 = v1+v2
 
-print(v3)
+print(v3.x)
+print(v3.y)
